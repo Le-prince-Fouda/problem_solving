8d25463f0f1ef7b2804a00f6b30a75295437f666
Le-prince-Fouda <fouda.franckau@yahoo.com>
2026-05-11 19:55:23 +0200

    update: addtext and comments

--- data_structure/linkedList/test_linkedList.py
+++ data_structure/linkedList/test_linkedList.py
@@ -1,9 +1,9 @@
 from linkedList import LinkedList
 
 l1 = LinkedList()
-print("Est vide?", l1.is_empty())
+print("Is empty?", l1.is_empty())
 
-#On insère 10 éléments dans la liste chaînée
+# Insert 10 elements into the linked list
 for i in range(10):
     l1.add_tail(i)
 
@@ -11,45 +11,45 @@ l1.print_list()
 l1.print_head_tail()
 print()
 
-print("Longueur :", l1.get_size())
-print("Est vide?", l1.is_empty())
-print("Noeud à l'indice 5 :", l1.get_node(5))
+print("Length:", l1.get_size())
+print("Is empty?", l1.is_empty())
+print("Node at index 5:", l1.get_node(5))
 
 print()
-print("On ajoute la valeur 50 à l'indice 5")
+print("Add the value 50 at index 5")
 l1.insert_at_position(5, 50)
 l1.print_list()
 l1.print_head_tail()
 #####
 
 print()
-print("On retire le 50")
+print("Remove 50")
 l1.remove(50)
 l1.print_list()
 l1.print_head_tail()
 
 print()
-print("On ajoute le 11 et le 10")
+print("Add 11 and 10")
 l1.add_tail( 11)
 l1.add_tail( 10)
 l1.print_list()
 l1.print_head_tail()
 
 print()
-print("On insère un 10 après le 5")
+print("Insert 10 after 5")
 l1.add_head( 10)
 l1.print_list()
 l1.print_head_tail()
 #######
 
 print()
-print("On retire le 10 après le 5")
+print("Remove the 10 after 5")
 l1.remove_by_index(6)
 l1.print_list()
 l1.print_head_tail()
 
 print()
-print("On retire le 9, le 10 et le 11")
+print("Remove 9, 10, and 11")
 l1.remove(9)
 l1.remove_tail()
 l1.remove_tail()
@@ -57,38 +57,39 @@ l1.print_list()
 l1.print_head_tail()
 
 print()
-print("On retire le 0")
+print("Remove 0")
 l1.remove(0)
 l1.print_list()
 l1.print_head_tail()
 # #######
 
-print("Longueur :", l1.get_size())
+print("Length:", l1.get_size())
 print()
-print("On retire tous les éléments allant de 1 à 5 inclus.")
-# On retire 5 fois l'élément qui se trouve à l'index 1
+print("Remove all elements from 1 to 5 inclusive.")
+# Remove the element at index 1 five times
 for _ in range(5):
     l1.remove_by_index(1)
 l1.print_list()
 l1.print_head_tail()
 
 print()
-print("On retire le premier élément")
+print("Remove the first element")
 l1.remove_head()
 l1.print_list()
 l1.print_head_tail()
 
 print()
-print("On retire le premier élément")
+print("Remove the first element")
 l1.remove_head()
 l1.print_list()
 l1.print_head_tail()
-print("Longueur :", l1.get_size())
+print("Length:", l1.get_size())
+
 
 print()
-print("On retire le premier élément")
+print("Remove the first element")
 l1.remove_head()
 l1.print_list()
 l1.print_head_tail()
-print("Longueur :", l1.get_size())
+print("Length:", l1.get_size())
 
